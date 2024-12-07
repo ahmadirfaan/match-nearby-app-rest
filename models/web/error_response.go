1b94aad0efd2dbfbfa78119a0ab41328cbcd3ac9
Ahmad Irfaan <irfaan.hibatullah@gmail.com>
2024-12-08 02:24:20 +0700

    add error response models

---
+++ models/web/error_response.go
@@ -0,0 +1,31 @@
+package web
+
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+    Message string `json:"message"`
+}
+
+
+func AuthError() ErrorResponse {
+    return newErrorResponse(http.StatusUnauthorized, "Authentication failed. Please check your credentials.")
+}
+
+func NotFoundError() ErrorResponse {
+    return newErrorResponse(http.StatusNotFound, "The requested resource was not found.")
+}
+
+func ForbiddenError() ErrorResponse {
+    return newErrorResponse(http.StatusForbidden, "You do not have permission to access this resource.")
+}
+
+func InternalServiceError() ErrorResponse {
+    return newErrorResponse(http.InternalServiceError, "Internal Server Error")
+}
+
+
+func newErrorResponse(code int, message string) ErrorResponse {
+    return ErrorResponse{
+        Code:    code,
+        Message: message,
+    }
+}
